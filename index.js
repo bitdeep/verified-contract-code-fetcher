7c55d70c111b6aac594310457026c69c0c154716
bitdeep <19378023+bitdeep@users.noreply.github.com>
2022-03-02 13:13:58 -0300

    corrections and bsc implementation

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const Web3 = require('web3');
 require('dotenv').config({path: '.env'});
 const { ethers } = require("ethers");
 
-process.on('uncaughtException', function (err) {
-    console.error(err);
-    //process.exit(1);
-});
-process.setMaxListeners(0);
+// prevent app crashing because of some internal exception
+    process.on('uncaughtException', function (err) {
+        console.error(err);
+        //process.exit(1);
+    });
+    process.setMaxListeners(0);
+    require('events').EventEmitter.defaultMaxListeners = 0;
 
-require('events').EventEmitter.defaultMaxListeners = 0;
 
+// standard contract detector and code fetcher, this is a generic evm block scan
+// that start from selected block to current block.
 async function extract(networkId, networkRpc, startBlock, apiEndpoint, getsourcecode, apiKey) {
     if( ! apiKey ){
         red(`Api key not available process.env.${networkId}`);
@@ -21,21 +24,22 @@ async function extract(networkId, networkRpc, startBlock, apiEndpoint, getsource
     const web3 = new Web3(networkRpc);
     const lastBlock = await web3.eth.getBlockNumber();
     for( let blockNumber = startBlock; blockNumber < lastBlock; blockNumber++){
-        // green(networkId, networkRpc, blockNumber, startBlock, i);
-        const block = await web3.eth.getBlock(blockNumber , true);
-        // console.log(blockNumber);
-        await processTransactions(blockNumber, block.transactions, apiEndpoint, getsourcecode, apiKey);
+        const block = await web3.eth.getBlock( blockNumber , true);
+        await processTransactions(networkId, blockNumber, block.transactions, apiEndpoint, getsourcecode, apiKey);
     }
 
 }
 
+// as on evm chain contract has a deterministic name generation,
+// we have a api for it.
 function getContractAddressFromTx(tx){
     const nonce = tx.nonce;
     const from = tx.from;
     return ethers.utils.getContractAddress({from, nonce});
 }
 
-async function processTransactions(blockNumber, transactions, apiEndpoint, getsourcecode, apiKey){
+// extract transactions for each block and check if the tx is a a contract creation
+async function processTransactions(networkId, blockNumber, transactions, apiEndpoint, getsourcecode, apiKey){
     const total = transactions.length;
     for( let txIndex = 0 ; txIndex < total ; txIndex ++ ){
         const tx = transactions[txIndex];
@@ -44,16 +48,19 @@ async function processTransactions(blockNumber, transactions, apiEndpoint, getso
             const deployer = tx.from;
             const contract = getContractAddressFromTx(tx);
             console.log(blockNumber, deployer, contract);
-            await getsourcecode(apiEndpoint, contract, apiKey)
+            await getsourcecode(networkId, apiEndpoint, contract, apiKey)
         }
     }
 }
 
+// daly of 1 second for each contract fetch
 async function delay(){
-    new Promise(resolve => setTimeout(resolve, 1000));
+    new Promise(resolve => setTimeout(resolve, 100));
 }
-async function etherscan(apiEndpoint, contractAddress, apiKey){
-    const urlEndpoint = `https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${apiKey}`;
+
+// implementation of contract code fetch for *scan block explorers
+async function etherscan(networkId, apiEndpoint, contractAddress, apiKey){
+    const urlEndpoint = `https://${apiEndpoint}/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${apiKey}`;
     const contractApiResponse = await axios.get(urlEndpoint);
     for( let i = 0 ; i < contractApiResponse.data.result.length ; i ++ ){
         const contractData = contractApiResponse.data.result[i];
@@ -63,22 +70,34 @@ async function etherscan(apiEndpoint, contractAddress, apiKey){
         }
         const SourceCode = contractData.SourceCode;
         const contractName = contractData.ContractName;
-        await saveContractData(contractAddress, contractName, SourceCode, ABI);
+        await saveContractData(networkId, contractAddress, contractName, SourceCode, ABI);
     }
     await delay();
 }
-async function saveContractData(contractAddress, contractName, source, abi ){
+
+// here is devops should implement what to do with the contract data,
+// for now, it just save in the file system
+async function saveContractData(networkId, contractAddress, contractName, source, abi ){
     const prefix = contractAddress.substring(0, 3);
-    const dirName = `./data/${prefix}/${contractAddress}`;
+    const dirName = `./data/${networkId}/${prefix}/${contractAddress}`;
     yellow(dirName, contractName);
     fs.mkdirSync(dirName, {recursive: true});
     fs.writeFileSync(`${dirName}/${contractName}.sol`, source);
     fs.writeFileSync(`${dirName}/${contractName}.abi`, abi);
 }
+
+// main runner, can run in parallel various scanners
 async function main() {
-    const rpc = `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`
-    const api = 'https://api.etherscan.io';
-    extract(1, rpc, 14307006, api, etherscan, process.env.API_ETHERSCAN);
+    const ethereumRpc = `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`
+    const bscRpc = `https://speedy-nodes-nyc.moralis.io/${process.env.MORALIS_API_KEY}/bsc/mainnet/archive`
+
+    // extract(1, ethereumRpc, 14307006, 'https://api.etherscan.io',
+    //     etherscan, process.env.API_ETHERSCAN);
+
+
+    // for bsc there is a bug, workaround is here https://github.com/ChainSafe/web3.js/issues/3912#issuecomment-1004045262
+    extract(56, bscRpc, 15710947, 'api.bscscan.com',
+        etherscan, process.env.API_BSCSCAN);
 }
 
 const chalk = require('chalk');
